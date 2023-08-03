256ac449a623495e524fea4fb6a7bbff3a7b235a
Lao <estanislaolanus@gmail.com>
2023-08-03 16:41:22 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Body from "./components/Body/Body"
+import Header from "./components/Header/Header"
+import Main from "./components/Main/Main"
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Main/>
+      <Body/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+
+export default function Header() {
+    return (
+        <header>
+            <nav className='navbar'>
+                <div className="logo">logo</div>
+                <ul>
+                    <li>Inicio</li>
+                    <li>Comunidad</li>
+                    <li>¿Que es sentir?</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
