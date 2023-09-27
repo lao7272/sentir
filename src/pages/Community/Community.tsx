d20c49887f82a92c1c15ead95235aabdf5d2ac90
Lao <estanislaolanus@gmail.com>
2023-09-27 18:44:47 -0300

    Added animation to community

--- src/pages/Community/Community.tsx
+++ src/pages/Community/Community.tsx
@@ -1,24 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { community } from '../../constants/constants'
 import { Section } from '../../types/types';
 export default function Community() {
     const [sections, setSections] = useState<Section[]>(community);
     const [sectionIndex, setSectionIndex] = useState<number>(-1);
-
+    const [animation, setAnimation] = useState<string>("");
+    useEffect(() => {
+        setAnimation("show-card .7s ease");
+    }, [sections]);
     const sectionFilter = (section: number) => {
-        setSectionIndex(section);
+        setSectionIndex(() => section);
         if (section === -1) {
-            setSections(community);
+            setSections(() => community);
             return;
         }
         const newSections: Section[] = community.filter(s => s.section === section);
         setSections(newSections);
     }
+    const onAnimationEnd = () => setAnimation("");
+    
     return (
         <div className="community">
-            <div className="community-title">
-
-            </div>
+            <div className="community-title"></div>
             <div className="community-nav">
                 <button onClick={() => sectionFilter(-1)} className={`community-section ${sectionIndex === -1 ? "active" : ""}`}>Comunidad</button>
                 <button onClick={() => sectionFilter(0)} className={`community-section ${sectionIndex === 0 ? "active" : ""}`}>Sentir 1.0</button>
@@ -33,7 +36,10 @@ export default function Community() {
                         return (
                             <div
                                 key={`${i}-community`}
-                                className={`community-card`}>
+                                style={{animation}}
+                                className="community-card"
+                                onAnimationEnd={() => onAnimationEnd()}
+                                >
                                 <div className="card-img">
                                     <img src={e.image} alt="image" />
                                 </div>
